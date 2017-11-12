821ff67f28c30f528f1a6f326ea2c65615fb32ec
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2017-11-13 00:27:16 +0200

    Authenticating properly

--- main.go
+++ main.go
@@ -7,46 +7,89 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"text/template"
 )
 
-const configFilePath = "$HOME/.config/getpkt/config.json"
+const (
+	configFilePath  = "$HOME/.config/getpkt/config.json"
+	authURLTemplate = "https://getpocket.com/auth/authorize?request_token={{.Code}}&redirect_uri={{.RedirectURL}}"
+)
 
-type oauthRequest struct {
+var defaultRedirectURL = "http://localhost:9998"
+
+type codeRequest struct {
 	ConsumerKey string `json:"consumer_key"`
-	RedirectURI string `json:"redirect_uri"`
+	RedirectURL string `json:"redirect_uri"`
 }
 
 type codeResponse struct {
 	Code string `json:"code"`
 }
 
-var config = struct {
+type authorizeRequest struct {
 	ConsumerKey string `json:"consumer_key"`
-}{}
+	Code        string `json:"code"`
+}
 
-func readConfig() (err error) {
+type authorizeResponse struct {
+	AccessToken string `json:"access_token"`
+	Username    string `json:"username"`
+}
+
+type appConfig struct {
+	ConsumerKey string `json:"consumer_key"`
+	AccessToken string `json:"access_token"`
+}
+
+func makeAuthURL(code, redirectURL string) string {
+	var data = struct {
+		Code        string
+		RedirectURL string
+	}{
+		url.QueryEscape(code),
+		url.QueryEscape(redirectURL),
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	tpl := template.Must(template.New("authURL").Parse(authURLTemplate))
+	err := tpl.Execute(buf, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buf.String()
+}
+
+func readConfig(path string) (config *appConfig, err error) {
 	var (
 		file *os.File
-		path string
 	)
-	path = os.ExpandEnv(configFilePath)
+	path = os.ExpandEnv(path)
+	config = &appConfig{}
 
 	file, err = os.Open(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	io := bufio.NewReader(file)
 	decoder := json.NewDecoder(io)
 	for decoder.More() {
-		err = decoder.Decode(&config)
+		err = decoder.Decode(config)
 		if err != nil {
-			break
+			return nil, err
 		}
 	}
 
-	return err
+	return config, err
+}
+
+func isReadable(path string) bool {
+	path = os.ExpandEnv(path)
+	_, err := os.Open(path)
+	return err == nil
 }
 
 func jsonRequest(url string, reqJSON interface{}, respJSON interface{}) (err error) {
@@ -95,41 +138,138 @@ func jsonRequest(url string, reqJSON interface{}, respJSON interface{}) (err err
 	return err
 }
 
-func authenticate(consumerKey string) (accessToken string, err error) {
+func obtainCode(consumerKey string) (code string, err error) {
 	var (
-		auth = oauthRequest{
-			config.ConsumerKey,
-			"http://localhost:9998",
+		request = codeRequest{
+			consumerKey,
+			defaultRedirectURL,
 		}
-		code         string
-		authResponse = codeResponse{}
+		response = codeResponse{}
 	)
 
 	err = jsonRequest(
 		"https://getpocket.com/v3/oauth/request",
-		&auth,
-		&authResponse,
+		&request,
+		&response,
 	)
 	if err != nil {
-		return "", fmt.Errorf("Failed to make a request for a code: %v", err)
+		return "", fmt.Errorf("Failed to obtain code: %v", err)
 	}
 
-	code = authResponse.Code
+	return response.Code, nil
+}
+
+func authorize(consumerKey, code string) (*authorizeResponse, error) {
+	var (
+		request = authorizeRequest{
+			consumerKey,
+			code,
+		}
+		response = authorizeResponse{}
+	)
+
+	err := jsonRequest(
+		"https://getpocket.com/v3/oauth/authorize",
+		&request,
+		&response,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to authorize: %v", err)
+	}
+
+	return &response, nil
+}
+
+func authenticate(consumerKey string) (accessToken string, err error) {
+	code, err := obtainCode(consumerKey)
 	if len(code) == 0 {
 		return "", fmt.Errorf("Received no code")
 	}
 
-	return accessToken, err
+	log.Println("Please proceed to:", makeAuthURL(code, defaultRedirectURL))
+	log.Print("Press enter when ready:")
+	bufio.NewReader(os.Stdin).ReadLine()
+
+	response, err := authorize(consumerKey, code)
+	if err != nil {
+		return "", fmt.Errorf("Failed to authorize: %v", err)
+	}
+
+	return response.AccessToken, nil
+}
+
+func showUsage() {
+	fmt.Printf(`
+Usage:
+	%s command
+
+Commands:
+	auth
+	list
+`, path.Base(os.Args[0]))
+}
+
+func readConsumerKey() string {
+	log.Print("Please enter consumer key: ")
+	line, _, err := bufio.NewReader(os.Stdin).ReadLine()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(line)
+}
+
+func writeConfig(path string, config *appConfig) (err error) {
+	file, err := os.OpenFile(
+		os.ExpandEnv(path),
+		os.O_WRONLY|os.O_CREATE,
+		0600,
+	)
+	if err != nil {
+		return err
+	}
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(config)
+}
+
+func initConfig() (config *appConfig) {
+	var err error
+
+	if isReadable(configFilePath) {
+		config, err = readConfig(configFilePath)
+		if err != nil {
+			log.Fatalf("Failed to read a config: %v", err)
+		}
+	} else {
+		config = &appConfig{
+			ConsumerKey: readConsumerKey(),
+		}
+		err := writeConfig(configFilePath, config)
+		if err != nil {
+			log.Fatalf("Failed to write a config: %v", err)
+		}
+	}
+
+	return config
 }
 
 func main() {
-	err := readConfig()
-	if err != nil {
-		log.Fatal(err)
+	switch {
+	case len(os.Args) == 1:
+		showUsage()
+		os.Exit(1)
+	case os.Args[1] == "auth":
+		config := initConfig()
+		accessToken, err := authenticate(config.ConsumerKey)
+		if err != nil {
+			log.Fatalf("Failed to authenticate: %v", err)
+		}
+		config.AccessToken = accessToken
+		writeConfig(configFilePath, config)
+		log.Println("Written to config")
+	case os.Args[1] == "list":
+	default:
+		showUsage()
+		os.Exit(1)
 	}
-	code, err := authenticate(config.ConsumerKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Code is ", code)
 }
